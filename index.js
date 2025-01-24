669fd1a83801068ee8b9fbcab39a97a17ab67d60
dilara369 <dilarahossenara@gmail.com>
2025-01-25 00:08:09 +0600

    get visa update

--- index.js
+++ index.js
@@ -65,20 +65,17 @@ async function run() {
      );
      res.send(result);
    });
-
    app.get("/visa-update/:id", async (req, res) => {
      const id = req.params.id;
      const query = { _id: new ObjectId(id) };
      const result = await visaCollection.findOne(query);
      res.send(result);
    });
-
    // PUT method to update VISA by ID
    app.put("/visa-update/:id", async (req, res) => {
      const visaId = req.params.id;
      const updatedData = req.body;
-     // console.log(updatedData);
-
+     
      const filter = { _id: new ObjectId(visaId) };
      const options = { upsert: true };
      const updatedDoc = {
